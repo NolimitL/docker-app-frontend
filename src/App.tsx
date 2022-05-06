9b803343c33149d40e9bbf90053aadefd4a5fd80
NolimitL <leonidprofessor27@gmail.com>
2022-05-06 14:13:31 +0200

    fix: small

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-          <h3>List of products!</h3>
+          <h3>List of products!!!</h3>
           <div>
               <form onSubmit={handleSubmit(handleOnSubmit)}>
                   <div>
